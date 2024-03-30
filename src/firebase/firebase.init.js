d12fa94f71c5dbe238ebc0bb4eeb711f26f88d75
Md Fahim Hossain <fahimreshad3@gmail.com>
2024-03-30 17:33:18 +0600

    this is react authentication basics practice

---
+++ src/firebase/firebase.init.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA_Nncn1iHixC5adxIc0wG_rpxT3Cgsg_Q",
+  authDomain: "simple-firebase-c7edd.firebaseapp.com",
+  projectId: "simple-firebase-c7edd",
+  storageBucket: "simple-firebase-c7edd.appspot.com",
+  messagingSenderId: "961605604231",
+  appId: "1:961605604231:web:9ea850349d8cfee6397922"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
